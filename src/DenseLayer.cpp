b0d96c6d191e0c2872b3ae65bce013af6714b9b6
Yashovardhan Jha <yashovardhan20@gmail.com>
2026-07-08 10:34:52 +0000

    implement He initialization for weights in DenseLayer constructor. closes #1

--- src/DenseLayer.cpp
+++ src/DenseLayer.cpp
@@ -1,8 +1,20 @@
 #include "DenseLayer.h"
+#include <random>
+#include <cmath>
 
 DenseLayer::DenseLayer(int n_in, int n_out)
 {
-    W = Eigen::MatrixXd::Random(n_in, n_out);
+    static std::mt19937 generator(std::random_device{}());
+
+    double stddev = std::sqrt(2.0 / n_in);
+    std::normal_distribution<double> dist(0.0, stddev);
+
+    W.resize(n_in, n_out);
+    for (int i = 0; i < W.rows(); ++i) {
+        for (int j = 0; j < W.cols(); ++j) {
+            W(i, j) = dist(generator);
+        }
+    }
     b = Eigen::VectorXd::Zero(n_out);
     gradW = Eigen::MatrixXd::Zero(n_in,n_out);
     gradb = Eigen::VectorXd::Zero(n_out);
